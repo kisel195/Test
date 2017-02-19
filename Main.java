6b0ed0abf6fb92cd647ca1fdc3a5d34f1b28b97e
kisel195 <miha_1585@mail.ru>
2017-02-20 02:56:07 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,16 +4,19 @@ public class Main {
 		
 	public static void main(String[] args) {
 		
-		/*try(FileReader reader = new FileReader("C:\\data.txt")) {
-	    int temp;
-	    	while((temp = reader.read()) != -1) {   
-	    		System.out.print((char)temp);
-	    	} 
-	    }
-	    
-	    catch(IOException ex) {
-	    	System.out.println(ex.getMessage());
-	    }*/
+		/*FileReader fr = new FileReader("D:/text.txt");
+		Scanner sc = new Scanner(fr);
+		Scanner scr;
+		sc.useDelimiter(", *");
+		ArrayList<Item> Items = new ArrayList<Item>();
+		for(int i = 0 ;sc.hasNext(); i++){
+		    s = sc.nextLine();
+		    scr = new Scanner(s);
+		    scr.useDelimiter(", *");
+		    for(int j = 0 ;scr.hasNext(); j++){
+			    a1.add(scr.nextInt());
+			    if (scr.hasNext()) a2.add(scr.nextInt());   }
+		}*/
 		
 		
 		GetCommand getCommand = new GetCommand();
